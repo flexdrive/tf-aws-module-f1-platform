a8a01dae18b0e366ede7e1d2712102c5df4cc03e
Tega McKinney <tega.mckinney@flexdrive.com>
2018-11-27 09:33:32 -0500

    Update dns-zone output

--- dns-zone/outputs.tf
+++ dns-zone/outputs.tf
@@ -1,14 +1,13 @@
 output "zone_id" {
-  value       = "${join("", aws_route53_zone.default.*.zone_id)}"
+  value       = "${aws_route53_zone.default.zone_id}"
   description = "Route53 DNS Zone ID"
 }
 
 output "zone_name" {
-  value       = "${replace(join("", aws_route53_zone.default.*.name), "/\\.$$/", "")}"
+  value       = "${aws_route53_zone.default.name}"
   description = "Route53 DNS Zone name"
 }
 
-output "fqdn" {
-  value       = "${join("", aws_route53_zone.default.*.name)}"
-  description = "Fully-qualified domain name"
+output "name_servers" {
+  value = "${join(",",aws_route53_zone.default.name_servers)}"
 }
